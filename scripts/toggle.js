d2bca79a2bf04aef5a54a88b7a504d069d3b5c34
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-26 19:31:35 +0600

    i have added toggle handler utility funcion to make code more reusable

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -5,22 +5,31 @@ const addMoneyFeature = document.getElementById("add-money-feature");
 const cashoutFeature = document.getElementById("cashout-feature");
 cashoutSection.style.display = "none";
 addMoneySection.style.display = "none";
+
 addMoneyFeature.addEventListener("click", function () {
-  latestPaymentSection.style.display = "none";
+  /* latestPaymentSection.style.display = "none";
   cashoutSection.style.display = "none";
   addMoneySection.style.display = "block";
 
   //feature border style
   addMoneyFeature.style.border = "2px solid skyblue";
-  cashoutFeature.style.border = "none";
+  cashoutFeature.style.border = "none"; */
+  // using toggle handler
+  toggleHandler("latest-payment", "none");
+  toggleHandler("cashout-container", "none", "cashout-feature", false);
+  toggleHandler("addMoney-container", "block", "add-money-feature", true);
 });
 
 cashoutFeature.addEventListener("click", function () {
-  latestPaymentSection.style.display = "none";
+  /* latestPaymentSection.style.display = "none";
   addMoneySection.style.display = "none";
   cashoutSection.style.display = "block";
 
   //feature border style
   cashoutFeature.style.border = "2px solid skyblue";
-  addMoneyFeature.style.border = "none";
+  addMoneyFeature.style.border = "none"; */
+
+  toggleHandler("latest-payment", "none");
+  toggleHandler("addMoney-container", "none", "add-money-feature", false);
+  toggleHandler("cashout-container", "block", "cashout-feature", true);
 });
--- utilities/utility-function.js
+++ utilities/utility-function.js
@@ -1,6 +1,16 @@
 function getInputValueById(id) {
   const value = document.getElementById(id).value;
   const valueAsNumber = parseInt(value);
-
   return valueAsNumber;
+}
+
+function toggleHandler(id, status, borderElm, border) {
+  const element = document.getElementById(id);
+  const borderElement = document.getElementById(borderElm);
+  element.style.display = status;
+  if (border) {
+    borderElement.style.border = "2px solid skyblue";
+  } else if (border !== undefined && border === false) {
+    borderElement.style.border = "none";
+  }
 }
