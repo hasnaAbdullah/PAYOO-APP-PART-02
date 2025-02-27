6d07e0b2c48d967249a6fde45466f6ad9b742393
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-27 09:34:58 +0600

    everything is ok

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -3,17 +3,20 @@ const cashoutSection = document.getElementById("cashout-container");
 const transferSection = document.getElementById("transfer-container");
 const billSection = document.getElementById("bill-container");
 const bonusSection = document.getElementById("bonus-container");
+const transactionSection = document.getElementById("transaction-container");
 const addMoneyFeature = document.getElementById("add-money-feature");
 const cashoutFeature = document.getElementById("cashout-feature");
 const transferFeature = document.getElementById("transfer-money-feature");
 const bonusFeature = document.getElementById("bonus-feature");
 const billFeature = document.getElementById("bill-feature");
+const transactionFeature = document.getElementById("transaction-feature");
 
 cashoutSection.style.display = "none";
 addMoneySection.style.display = "none";
 transferSection.style.display = "none";
 bonusSection.style.display = "none";
 billSection.style.display = "none";
+transactionSection.style.display = "none";
 
 addMoneyFeature.addEventListener("click", function () {
   toggleHandler("latest-payment", "none");
@@ -21,6 +24,7 @@ addMoneyFeature.addEventListener("click", function () {
   toggleHandler("transfer-container", "none", "transfer-money-feature", false);
   toggleHandler("bonus-container", "none", "bonus-feature", false);
   toggleHandler("bill-container", "none", "bill-feature", false);
+  toggleHandler("transaction-container", "none", "transaction-feature", false);
   toggleHandler("addMoney-container", "block", "add-money-feature", true);
 });
 
@@ -30,6 +34,7 @@ cashoutFeature.addEventListener("click", function () {
   toggleHandler("transfer-container", "none", "transfer-money-feature", false);
   toggleHandler("bonus-container", "none", "bonus-feature", false);
   toggleHandler("bill-container", "none", "bill-feature", false);
+  toggleHandler("transaction-container", "none", "transaction-feature", false);
   toggleHandler("cashout-container", "block", "cashout-feature", true);
 });
 
@@ -39,6 +44,7 @@ transferFeature.addEventListener("click", function () {
   toggleHandler("cashout-container", "none", "cashout-feature", false);
   toggleHandler("bonus-container", "none", "bonus-feature", false);
   toggleHandler("bill-container", "none", "bill-feature", false);
+  toggleHandler("transaction-container", "none", "transaction-feature", false);
   toggleHandler("transfer-container", "block", "transfer-money-feature", true);
 });
 bonusFeature.addEventListener("click", function () {
@@ -47,6 +53,7 @@ bonusFeature.addEventListener("click", function () {
   toggleHandler("cashout-container", "none", "cashout-feature", false);
   toggleHandler("transfer-container", "none", "transfer-money-feature", false);
   toggleHandler("bill-container", "none", "bill-feature", false);
+  toggleHandler("transaction-container", "none", "transaction-feature", false);
   toggleHandler("bonus-container", "block", "bonus-feature", true);
 });
 billFeature.addEventListener("click", function () {
@@ -55,5 +62,15 @@ billFeature.addEventListener("click", function () {
   toggleHandler("cashout-container", "none", "cashout-feature", false);
   toggleHandler("transfer-container", "none", "transfer-money-feature", false);
   toggleHandler("bonus-container", "none", "bonus-feature", false);
+  toggleHandler("transaction-container", "none", "transaction-feature", false);
   toggleHandler("bill-container", "block", "bill-feature", true);
+});
+transactionFeature.addEventListener("click", function () {
+  toggleHandler("latest-payment", "none");
+  toggleHandler("addMoney-container", "none", "add-money-feature", false);
+  toggleHandler("cashout-container", "none", "cashout-feature", false);
+  toggleHandler("transfer-container", "none", "transfer-money-feature", false);
+  toggleHandler("bonus-container", "none", "bonus-feature", false);
+  toggleHandler("bill-container", "none", "bill-feature", false);
+  toggleHandler("transaction-container", "block", "transaction-feature", true);
 });
